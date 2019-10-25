36259329ad21f48d2d59ade1b1bd205b8cadaf42
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-25 13:05:26 +0800

    Start using argv

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const chalk = require('chalk');
 const notes = require('./notes');
 
-console.log(notes.getNOtes());
+const command = process.argv[2];
 
-console.log(chalk.green.bgBlack.bold.inverse('Success!'));
+console.log(process.argv);
+
+if (command === 'add') {
+  console.log(chalk.white.bgBlue.bold.inverse('Adding Note'));
+} else if (command === 'remove') {
+  console.log(chalk.white.bgRed.bold('Removing Note'));
+}
